d7dcd32a6065eb72a87668b0910f2fb258790263
Bradley Jensen <bradley13@gmail.com>
2020-05-14 08:44:43 -0400

    starting BlizzardApiInterface

--- BlizzardApiInterface/BlizzardApiInterface.js
+++ BlizzardApiInterface/BlizzardApiInterface.js
@@ -14,14 +14,31 @@ class BlizzardApiInterface {
     this.clientId = clientId;
     this.clientSecret = clientSecret;
     this.accessToken = null;
+    this.axiosInstance = null;
+    this.namespace = "dynamic-us";
+    this.locale = "en_US";
   }
 
-  tokenUrl = `https://us.battle.net/oauth/token`;
+  initialize = function () {
+    console.log("initialize start");
+    return new Promise((resolve, reject) => {
+      this.getToken().then((res) => {
+        console.log("resolve getToken");
+        this.accessToken = new AccessToken(
+          res.data.access_token,
+          res.data.token_type,
+          res.data.expires_in
+        );
+        this.setAxiosInstance();
+        resolve(true);
+      });
+    });
+  };
 
-  initialize = new Promise((resolve, reject) => {
-    axios({
+  getToken = function () {
+    return axios({
       method: "post",
-      url: this.tokenUrl,
+      url: process.env.BLIZZARD_TOKEN_URL,
       auth: {
         username: process.env.BLIZZARD_CLIENT_ID,
         password: process.env.BLIZZARD_CLIENT_SECRET,
@@ -29,24 +46,34 @@ class BlizzardApiInterface {
       params: {
         grant_type: "client_credentials",
       },
-    })
-      .then((res) => {
-        this.accessToken = new AccessToken(
-          res.data.access_token,
-          res.data.token_type,
-          res.data.expires_in
-        );
-        this.axiosInstance = axios.create({
-          baseURL: 'https://some-domain.com/api/',
-          timeout: 1000,
-          headers: {'Authorization': `Bearer ${this.accessToken.accessToken}`}
-        });
-        resolve(true);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+    });
+  };
+
+  getConnectedRealmsIndex = async function () {
+    let dataPromise = new Promise((resolve, reject) => {
+      this.axiosInstance
+        .get(
+          `data/wow/connected-realm/index?namespace=${this.namespace}&local=${this.locale}`
+        )
+        .then((result) => {
+          resolve(result.data);
+        })
+        .catch((error) => reject(error));
+    });
+    let data = await dataPromise;
+    return data;
+  };
+
+  setAxiosInstance = function () {
+    this.axiosInstance = axios.create({
+      baseURL: "https://us.api.blizzard.com",
+      timeout: 1000,
+      headers: {
+        Authorization: `Bearer ${this.accessToken.accessToken}`,
+        "If-Modified-Since": "header",
+      },
+    });
+  };
 }
 
 module.exports.AccessToken = AccessToken;
--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
-const {AccessToken, BlizzardApiInterface} = require("./BlizzardApiInterface/BlizzardApiInterface");
+const {
+  AccessToken,
+  BlizzardApiInterface,
+} = require("./BlizzardApiInterface/BlizzardApiInterface");
 
 bai = new BlizzardApiInterface(
   process.env.BLIZZARD_CLIENT_ID,
   process.env.BLIZZARD_CLIENT_SECRET
-); 
+);
 
-bai.initialize.then(() => {
-  console.log(bai.accessToken);
-});
+bai
+  .initialize()
+  .then((result) => {
+    bai.getConnectedRealmsIndex().then(console.log);
+  })
+  .catch((error) => console.log(error));
